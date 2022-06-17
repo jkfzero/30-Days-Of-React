ba43bcc9d356c43d9c6abe2ac3c16658ec657ad1
jkfzero <jkfzero@qq.com>
2022-06-17 21:29:14 +0800

    demo：mp4 in img tag work in safari

---
+++ answer/d03/leve2/myexec/pages/image_mp4.js
@@ -0,0 +1,7 @@
+export default function Image_mp4() {
+  return (
+    <div className="container rounded-lg m-6 p-24 bg-sky-200">
+      <img src="https://file-examples.com/storage/fe38cbe14062ab91d9edafb/2017/04/file_example_MP4_480_1_5MG.mp4" alt="picsum" className="rounded-lg" />
+    </div>
+  );
+}
